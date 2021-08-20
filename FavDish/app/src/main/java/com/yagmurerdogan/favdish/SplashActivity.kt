2fec9165c4a80050de072714e526c3a8eeab789c
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-08-21 00:12:11 +0300

    Check SDK version and remove status bar

--- FavDish/app/src/main/java/com/yagmurerdogan/favdish/SplashActivity.kt
+++ FavDish/app/src/main/java/com/yagmurerdogan/favdish/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.yagmurerdogan.favdish
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.yagmurerdogan.favdish.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -10,6 +13,16 @@ class SplashActivity : AppCompatActivity() {
 
         val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(splashBinding.root)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
 
     }
 }
