115bfd79de4eb85e86d4c6539fccc7814977a78f
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-08-21 00:30:19 +0300

    Add animation to splash screen

--- FavDish/app/src/main/java/com/yagmurerdogan/favdish/SplashActivity.kt
+++ FavDish/app/src/main/java/com/yagmurerdogan/favdish/SplashActivity.kt
@@ -1,10 +1,15 @@
 package com.yagmurerdogan.favdish
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.yagmurerdogan.favdish.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -23,6 +28,27 @@ class SplashActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
+
+        val splashAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_splash)
+        splashBinding.tvAppName.animation = splashAnimation
+
+        splashAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(p0: Animation?) {
+                //
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                }, 1000)
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {
+                TODO("Not yet implemented")
+            }
+
+        })
 
     }
 }
