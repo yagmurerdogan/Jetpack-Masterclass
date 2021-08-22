db97aeeedde2b177145bfa98b4cdbbeb6708459f
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-08-22 13:06:02 +0300

    Create new activity for Add Update Dish screen

---
+++ FavDish/app/src/main/java/com/yagmurerdogan/favdish/view/activities/AddUpdateDishActivity.kt
@@ -0,0 +1,11 @@
+package com.yagmurerdogan.favdish.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddUpdateDishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_update_dish)
+    }
+}
