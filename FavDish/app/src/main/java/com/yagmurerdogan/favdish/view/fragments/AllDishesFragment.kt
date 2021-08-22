c6c2ceb67083d7e004d244a46b8dbcb230420c4f
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-08-22 13:53:12 +0300

    Add menu to all dishes fragment

--- FavDish/app/src/main/java/com/yagmurerdogan/favdish/view/fragments/AllDishesFragment.kt
+++ FavDish/app/src/main/java/com/yagmurerdogan/favdish/view/fragments/AllDishesFragment.kt
@@ -1,14 +1,15 @@
 package com.yagmurerdogan.favdish.view.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.yagmurerdogan.favdish.R
 import com.yagmurerdogan.favdish.databinding.FragmentAllDishesBinding
+import com.yagmurerdogan.favdish.view.activities.AddUpdateDishActivity
 import com.yagmurerdogan.favdish.viewmodel.home.HomeViewModel
 
 class AllDishesFragment : Fragment() {
@@ -19,6 +20,11 @@ class AllDishesFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,6 +42,21 @@ class AllDishesFragment : Fragment() {
             textView.text = it
         })
         return root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_all_dishes, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_add_dish -> {
+                startActivity(Intent(requireActivity(), AddUpdateDishActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onDestroyView() {
