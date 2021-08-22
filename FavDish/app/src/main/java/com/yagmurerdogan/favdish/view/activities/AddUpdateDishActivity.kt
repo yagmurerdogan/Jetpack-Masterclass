16cb323244664cc9b65c61087c278a8f7e62f5ac
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-08-22 17:00:53 +0300

    Setup dialog on AddUppdateDishActivity

--- FavDish/app/src/main/java/com/yagmurerdogan/favdish/view/activities/AddUpdateDishActivity.kt
+++ FavDish/app/src/main/java/com/yagmurerdogan/favdish/view/activities/AddUpdateDishActivity.kt
@@ -1,11 +1,13 @@
 package com.yagmurerdogan.favdish.view.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.yagmurerdogan.favdish.R
 import com.yagmurerdogan.favdish.databinding.ActivityAddUpdateDishBinding
+import com.yagmurerdogan.favdish.databinding.DialogCustomImageSelectionBinding
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -33,10 +35,27 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
         if (p0 != null) {
             when (p0.id) {
                 R.id.iv_add_dish_image -> {
-                    Toast.makeText(this, "You've clicked the ImageView", Toast.LENGTH_SHORT).show()
+                    customImageSelectionDialog()
                     return
                 }
             }
         }
+    }
+
+    private fun customImageSelectionDialog() {
+        val dialog = Dialog(this)
+        val binding: DialogCustomImageSelectionBinding =
+            DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+        binding.tvCamera.setOnClickListener {
+            Toast.makeText(this, "Camera Clicked", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+        binding.tvGallery.setOnClickListener {
+            Toast.makeText(this, "Gallery Clicked", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+        dialog.show()
+
     }
 }
