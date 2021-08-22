5869b45637c4300bda80df2608163eccd613e35f
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-08-22 16:09:00 +0300

    Setup add dish toolbar and back button

--- FavDish/app/src/main/java/com/yagmurerdogan/favdish/view/activities/AddUpdateDishActivity.kt
+++ FavDish/app/src/main/java/com/yagmurerdogan/favdish/view/activities/AddUpdateDishActivity.kt
@@ -3,10 +3,25 @@ package com.yagmurerdogan.favdish.view.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yagmurerdogan.favdish.R
+import com.yagmurerdogan.favdish.databinding.ActivityAddUpdateDishBinding
 
 class AddUpdateDishActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
