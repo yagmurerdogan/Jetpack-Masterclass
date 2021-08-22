c4c2bb8847054eacc4c12a90283d0152c5ad52e8
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-08-22 13:21:49 +0300

    Change home activity and fragment name to all dishes

--- FavDish/app/src/main/java/com/yagmurerdogan/favdish/view/activities/AddUpdateDishActivity.kt
+++ FavDish/app/src/main/java/com/yagmurerdogan/favdish/view/activities/AddUpdateDishActivity.kt
@@ -2,6 +2,7 @@ package com.yagmurerdogan.favdish.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yagmurerdogan.favdish.R
 
 class AddUpdateDishActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
