5f7ef6085c5c6a0b30b63621680dd4792335fd44
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-08-23 18:34:56 +0300

    Create CustomListdapter.kt class

---
+++ FavDish/app/src/main/java/com/yagmurerdogan/favdish/view/adapters/CustomListItemAdapter.kt
@@ -0,0 +1,34 @@
+package com.yagmurerdogan.favdish.view.adapters
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.yagmurerdogan.favdish.databinding.ItemCustomListBinding
+
+class CustomListItemAdapter(
+    private val activity: Activity,
+    private val listItems: List<String>,
+    private val selection: String
+) : RecyclerView.Adapter<CustomListItemAdapter.ViewHolder>() {
+
+    class ViewHolder(view: ItemCustomListBinding) : RecyclerView.ViewHolder(view.root) {
+        val tvText = view.tvText
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding: ItemCustomListBinding = ItemCustomListBinding
+            .inflate(LayoutInflater.from(activity), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = listItems[position]
+        holder.tvText.text = item
+    }
+
+    override fun getItemCount(): Int {
+        return listItems.size
+    }
+
+}
