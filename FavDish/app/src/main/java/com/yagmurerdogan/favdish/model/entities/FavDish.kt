c59cf8a1d773c240174863749e75854b93409684
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-08-23 19:48:12 +0300

    Setup entities

---
+++ FavDish/app/src/main/java/com/yagmurerdogan/favdish/model/entities/FavDish.kt
@@ -0,0 +1,21 @@
+package com.yagmurerdogan.favdish.model.entities
+
+import android.media.Image
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "fav_dishes_table")
+data class FavDish(
+    @ColumnInfo val image: String,
+    @ColumnInfo(name = "image_source") val imageSource: String,
+    @ColumnInfo val title: String,
+    @ColumnInfo val type: String,
+    @ColumnInfo val category: String,
+    @ColumnInfo val ingredients: String,
+    @ColumnInfo(name = "cooking_time") val cookingTime: String,
+    @ColumnInfo(name = "instructions") val directionToCook: String,
+    @ColumnInfo(name = "favorite_dish") var favoriteDish : Boolean = false,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0
+
+)
