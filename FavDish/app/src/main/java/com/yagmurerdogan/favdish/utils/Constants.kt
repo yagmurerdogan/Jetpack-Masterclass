55c076fe4626d6ff6b32b6b8299ab4a4a2e2f78b
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-08-23 15:58:52 +0300

    Create util package and constants class

---
+++ FavDish/app/src/main/java/com/yagmurerdogan/favdish/utils/Constants.kt
@@ -0,0 +1,63 @@
+package com.yagmurerdogan.favdish.utils
+
+object Constants {
+
+    const val DISH_TYPE: String = "DishType"
+    const val DISH_CATEGORY: String = "DishCategory"
+    const val DISH_COOKING_TIME: String = "DishCookingTime"
+
+    fun dishTypes(): ArrayList<String> {
+        val list = ArrayList<String>()
+
+        list.add("breakfast")
+        list.add("lunch")
+        list.add("snacks")
+        list.add("dinner")
+        list.add("salad")
+        list.add("side dish")
+        list.add("desert")
+        list.add("other")
+
+        return list
+    }
+
+    fun dishCategories(): ArrayList<String> {
+        val list = ArrayList<String>()
+
+        list.add("Pizza")
+        list.add("BBQ")
+        list.add("Bakery")
+        list.add("Burger")
+        list.add("Cafe")
+        list.add("Chicken")
+        list.add("Dessert")
+        list.add("Drinks")
+        list.add("Hot Dogs")
+        list.add("Juices")
+        list.add("Sandwich")
+        list.add("Tea & Coffee")
+        list.add("Pizza")
+        list.add("Pizza")
+
+        return list
+    }
+
+    fun dishCookTime(): ArrayList<String> {
+        val list = ArrayList<String>()
+
+        list.add("10")
+        list.add("15")
+        list.add("20")
+        list.add("30")
+        list.add("45")
+        list.add("50")
+        list.add("60")
+        list.add("90")
+        list.add("120")
+        list.add("150")
+        list.add("180")
+
+        return list
+
+    }
+}
