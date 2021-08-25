d7265c7ce9b8c16a9a66e7783f5dee949580c2c8
Yağmur Erdoğan <yagmur.erdogan.1999@gmail.com>
2021-08-25 17:12:46 +0300

    Create functions to show and hide bottom bar

--- FavDish/app/src/main/java/com/yagmurerdogan/favdish/view/activities/MainActivity.kt
+++ FavDish/app/src/main/java/com/yagmurerdogan/favdish/view/activities/MainActivity.kt
@@ -3,8 +3,10 @@ package com.yagmurerdogan.favdish.view.activities
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.yagmurerdogan.favdish.R
@@ -12,17 +14,18 @@ import com.yagmurerdogan.favdish.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var mbinding: ActivityMainBinding
+    private lateinit var mNavController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        mbinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mbinding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        val navView: BottomNavigationView = mbinding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        mNavController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
@@ -30,7 +33,21 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_all_dishes, R.id.navigation_favorite_dishes, R.id.navigation_random_dish
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        setupActionBarWithNavController(mNavController, appBarConfiguration)
+        navView.setupWithNavController(mNavController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(mNavController, null)
+    }
+
+    fun hideButtonNavigationView() {
+        mbinding.navView.clearAnimation()
+        mbinding.navView.animate().translationY(mbinding.navView.height.toFloat()).duration = 300
+    }
+
+    fun showButtonNavigationView() {
+        mbinding.navView.clearAnimation()
+        mbinding.navView.animate().translationY(0f).duration = 300
     }
 }
