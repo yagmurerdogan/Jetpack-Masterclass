4ae81714e3daca06cec76ddcb4cdedc6f3c29f93
Yağmur Erdoğan <yagmur.erdogan.1999@gmail.com>
2021-08-26 10:38:29 +0300

    Add popup menu to dish item

--- FavDish/app/src/main/java/com/yagmurerdogan/favdish/view/adapters/FavDishAdapter.kt
+++ FavDish/app/src/main/java/com/yagmurerdogan/favdish/view/adapters/FavDishAdapter.kt
@@ -1,10 +1,14 @@
 package com.yagmurerdogan.favdish.view.adapters
 
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.yagmurerdogan.favdish.R
 import com.yagmurerdogan.favdish.databinding.ItemDishLayoutBinding
 import com.yagmurerdogan.favdish.model.entities.FavDish
 import com.yagmurerdogan.favdish.view.fragments.AllDishesFragment
@@ -14,6 +18,12 @@ class FavDishAdapter(private val fragment: Fragment) :
     RecyclerView.Adapter<FavDishAdapter.ViewHolder>() {
 
     private var dishes: List<FavDish> = listOf()
+
+    class ViewHolder(view: ItemDishLayoutBinding) : RecyclerView.ViewHolder(view.root) {
+        val ivDishImage = view.ivDishImage
+        val tvTitle = view.tvDishTitle
+        val ibMore = view.ibMore
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding: ItemDishLayoutBinding =
@@ -40,6 +50,26 @@ class FavDishAdapter(private val fragment: Fragment) :
                 fragment.dishDetails(dish)
             }
         }
+        holder.ibMore.setOnClickListener {
+            val popup = PopupMenu(fragment.context, holder.ibMore)
+            popup.menuInflater.inflate(R.menu.menu_adapter, popup.menu)
+
+            popup.setOnMenuItemClickListener {
+                if (it.itemId == R.id.action_edit_dish) {
+                    Log.i("You have clicked on", "Edit option of ${dish.title}")
+                } else if (it.itemId == R.id.action_delete_dish) {
+                    Log.i("You have clicked on", "Edit option of ${dish.title}")
+                }
+                true
+            }
+
+            popup.show()
+        }
+        if (fragment is AllDishesFragment) {
+            holder.ibMore.visibility = View.VISIBLE
+        } else if (fragment is FavoriteDishesFragment) {
+            holder.ibMore.visibility = View.GONE
+        }
     }
 
     override fun getItemCount(): Int {
@@ -51,8 +81,4 @@ class FavDishAdapter(private val fragment: Fragment) :
         notifyDataSetChanged()
     }
 
-    class ViewHolder(view: ItemDishLayoutBinding) : RecyclerView.ViewHolder(view.root) {
-        val ivDishImage = view.ivDishImage
-        val tvTitle = view.tvDishTitle
-    }
 }
